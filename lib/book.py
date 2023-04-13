14e2078009c8294a2b3d4f5f8fd606f10ebb2a4f
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-04-12 20:41:32 -0600

    super dooper

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    
+    def __init__(self, title, page_count=0):
+       self.title = title
+       self.page_count = page_count
+       
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+      
+        
+    def get_page_count(self):
+        return self._page_count
+        
+    def set_page_count(self, number):
+        
+        if type(number) == int:
+            self._page_count = number
+        
+        print("page_count must be an integer")
+        
+        
+    page_count = property(get_page_count, set_page_count)
+    
+   
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, size=0):
+        self.brand = brand
+        self._size = size
+    
+    def get_color(self):
+        return self.color
+    
+    def set_color(self, color):
+        self.color = color
+        
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, number):
+        if type(number) == int:
+            self._size = number
+      
+        print("size must be an integer")
+        
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+        
+    size = property(get_size, set_size)
